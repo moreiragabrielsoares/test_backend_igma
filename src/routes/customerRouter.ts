f67b059ced6370f28e70578fa514c74f007deeb6
Gabriel Soares Moreira <moreira.gabrielsoares@gmail.com>
2023-02-03 12:42:09 -0300

    feat: create customerRouter

---
+++ src/routes/customerRouter.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { validateSchema } from '../middlewares/schemaValidatorMiddleware';
+import { createNewCustomerSchema, getCustomerByCpfSchema } from '../schemas/customerSchemas';
+import * as customerController from '../controllers/customerController';
+
+const customerRouter = Router();
+
+customerRouter.post(
+  '/create-customer',
+  validateSchema(createNewCustomerSchema),
+  customerController.registerNewCustomer
+);
+customerRouter.get('/customer', validateSchema(getCustomerByCpfSchema), customerController.getCustomerByCpf);
+customerRouter.get('/allCustomers', customerController.getAllCustomers);
+
+export { customerRouter };
