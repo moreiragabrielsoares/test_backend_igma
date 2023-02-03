72cad137ab7faa1663d794be0c5fe8cc4f485443
Gabriel Soares Moreira <moreira.gabrielsoares@gmail.com>
2023-02-03 12:42:59 -0300

    feat: add customerRouter to app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { json } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errorHandler } from './middlewares/errorHandlerMiddleware';
+import { customerRouter } from './routes/customerRouter';
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(customerRouter);
+app.use(errorHandler);
 
 export { app };
